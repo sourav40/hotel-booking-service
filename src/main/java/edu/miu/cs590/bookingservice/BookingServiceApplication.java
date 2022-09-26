9a8e896beffb70f3bae6f692d52d629914c87425
sourav40 <souravshrestha40@gmail.com>
2022-09-26 16:17:45 -0500

    kafka consumer added.

--- src/main/java/edu/miu/cs590/bookingservice/BookingServiceApplication.java
+++ src/main/java/edu/miu/cs590/bookingservice/BookingServiceApplication.java
@@ -1,13 +1,26 @@
 package edu.miu.cs590.bookingservice;
 
+import edu.miu.cs590.bookingservice.kafka.consumer.PaymentConsumer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class BookingServiceApplication {
+public class BookingServiceApplication implements CommandLineRunner {
+
+    @Autowired
+    private PaymentConsumer paymentConsumer;
 
     public static void main(String[] args) {
-        SpringApplication.run(BookingServiceApplication.class, args);
+        new SpringApplicationBuilder(BookingServiceApplication.class)
+                .sources(BookingServiceApplication.class)
+                .run(args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        paymentConsumer.consumeMessage();
+    }
 }
--- src/main/java/edu/miu/cs590/bookingservice/kafka/consumer/PaymentConsumer.java
+++ src/main/java/edu/miu/cs590/bookingservice/kafka/consumer/PaymentConsumer.java
@@ -1,9 +1,82 @@
 package edu.miu.cs590.bookingservice.kafka.consumer;
 
-import org.springframework.context.annotation.Configuration;
+import edu.miu.cs590.bookingservice.dto.PaymentResponseDto;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
-@Configuration
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+@Slf4j
+@Service
 public class PaymentConsumer {
 
+    @Value("${kafka-server-url}")
+    private String kafkaUrl;
+
+    @Value("${kafka-server-port}")
+    private String kafkaPort;
+
+    public void consumeMessage() {
+
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaUrl + ":" + kafkaPort);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, " edu.miu.cs590.bookingservice.deserializer.PaymentResponseDeserializer");
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "email-payment-consumer-group"); // consumer needs to be in certain group
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); // read for the first offset
+
+        KafkaConsumer<String, PaymentResponseDto> consumer = new KafkaConsumer<>(properties);
+
+        final Thread mainThread = Thread.currentThread();
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                log.info("Detected a shutdown, let's exit by calling consumer.wakeup()...");
+                consumer.wakeup();
+
+                try {
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        try {
+            consumer.subscribe(Arrays.asList("payment_response"));
+
+            while (true) {
+                log.info("polling...");
+                ConsumerRecords<String, PaymentResponseDto> records = consumer.poll(Duration.ofMillis(1000));
+
+                for (ConsumerRecord<String, PaymentResponseDto> record : records) {
+                    log.info("Value: " + record.value() + "\n" +
+                            "Partition: " + record.partition() + "\n" +
+                            "Offset: " + record.offset() + "\n"
+                    );
+                    PaymentResponseDto paymentResponseDto = record.value();
+                    log.info("Payment response ::" + paymentResponseDto);
+                    // to do -> update status in the payment table
+                }
+            }
+        } catch (WakeupException e) {
+            log.info("Wake up exception!!!");
+            // this is an expected exception while trying to close the consumer
+        } catch (Exception e) {
+            log.error("Unexpected exception.");
+        } finally {
+            consumer.close();
+            log.info("The consumer in now gracefully closed.");
+        }
+    }
 
 }
