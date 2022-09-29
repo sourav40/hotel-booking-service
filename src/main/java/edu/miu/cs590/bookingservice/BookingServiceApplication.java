1862a9ac11431c0471b7c39f740702da452325ce
sourav40 <souravshrestha40@gmail.com>
2022-09-29 05:52:02 -0500

    files changed.

--- src/main/java/edu/miu/cs590/bookingservice/BookingServiceApplication.java
+++ src/main/java/edu/miu/cs590/bookingservice/BookingServiceApplication.java
@@ -5,9 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class BookingServiceApplication implements CommandLineRunner {
 
     @Autowired
